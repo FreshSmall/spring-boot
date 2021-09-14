8f8f446ef2ac5dbf9fabc24b7f5bc1fe78d4b6e3
yinchao <ycsuper2819@gmail.com>
2021-09-14 19:22:18 +0800

    spring boot 源码测试用例

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-jsp/src/main/java/dto/User.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) GSX Techedu Inc. All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package dto;
+
+/**
+ * @author yinchao
+ * @description
+ * @team wuhan operational dev.
+ * @date 2021/9/14 15:54
+ **/
+public class User {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
